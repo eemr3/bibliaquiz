b4fd3dabc438c2d77f0c96f88e704b3fcc424c37
Emerson <eemr3@yahoo.com.br>
2021-02-08 19:45:05 -0300

    Adicionando logo personalizado

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-import QuizScreen from '../../src/screen/Quiz'
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import QuizScreen from "../../src/screen/Quiz";
 export default function QuizDaGaleraPage({ dbExterno }) {
   return (
     <ThemeProvider theme={dbExterno.theme}>
@@ -9,30 +9,30 @@ export default function QuizDaGaleraPage({ dbExterno }) {
         externalBg={dbExterno.bg}
       />
     </ThemeProvider>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
-  const [projeto, usuario] = context.query.id.split('___')
+  const [projeto, usuario] = context.query.id.split("___");
   const dbExterno = await fetch(
     `https://${projeto}.${usuario}.vercel.app/api/db`
   )
     .then((response) => {
       if (response.ok) {
-        return response.json()
+        return response.json();
       }
-      throw new Error('Falha na requisição dos dados!')
+      throw new Error("Falha na requisição dos dados!");
     })
     .then((response) => {
-      return response
+      return response;
     })
     .catch((err) => {
-      console.error(err)
-    })
+      console.error(err);
+    });
 
   return {
     props: {
       dbExterno,
     },
-  }
+  };
 }
